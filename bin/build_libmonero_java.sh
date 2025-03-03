201815a5fd39e438a8142ca7ff0e652ccecb1d82
Brandon Trussell <brandon2k3@gmail.com>
2025-03-03 13:02:29 +0000

    wip for buidling fat-mac-lib

--- bin/build_libmonero_java.sh
+++ bin/build_libmonero_java.sh
@@ -1,20 +1,69 @@
-#!/bin/sh
+#!/bin/bash
 
 #EMCC_DEBUG=1
 
+CURRENT_ARCH=`uname -m`
+CUR_OS=`uname -s`
 HOST_NCORES=$(nproc 2>/dev/null || shell nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
 
+    CPU=""
+    if [ -z "${ARCH}" ]; then
+        CPU=$CURRENT_ARCH 
+    else
+        CPU="${ARCH}"
+    fi
+
+mkdir -p ./external-libs/monero-cpp/
+
+# VENDOR=""
+# if [ -n "$TARGET" ]; then 
+#     VENDOR=$TARGET
+# elif [ $CUR_OS == "Linux" ]; then
+#     VENDOR="linux"
+# else
+#     VENDOR="apple"
+# fi
+# 
+# OS=$CUR_OS
+# if [ VENDOR == ]; then 
+#     VENDOR=$TARGET
+# fi
+
+#     VERSION="${CURRENT_ARCH}-apple-${CUR_OS}"
+#         VERSION="${CPU}-${VENDOR}-${OS}" 
+
+if [ $CUR_OS == "Linux" ]; then
+    # build libmonero-cpp shared library
+    cd ./external/monero-cpp/
+    ARCH=$ARCH SKIP_MP=$SKIP_MP TARGET=$TARGET ./bin/build_libmonero_cpp.sh &&
+    cd ../.. &&
+    cp external/monero-cpp/build/libmonero-cpp* ./external-libs/monero-cpp/ 
+
+    # build libmonero-java shared library to ./build
+    mkdir -p ./build &&
+    cd build && 
+    cmake .. && 
+    cmake --build . -j$HOST_NCORES && 
+    make .
+
+
+
+
+
+
+else
+# Not running on Linux
+# Only build for the current platform
 # build libmonero-cpp shared library
 cd ./external/monero-cpp/ && 
 ./bin/build_libmonero_cpp.sh &&
-
-# copy libmonero-cpp shared library to ./build
 cd ../../ &&
-mkdir -p ./build &&
-cp ./external/monero-cpp/build/libmonero-cpp.* ./build &&
+cp ./external/monero-cpp/build/libmonero-cpp* ./external-libs/monero-cpp/ &&
 
 # build libmonero-java shared library to ./build
+mkdir -p ./build &&
 cd build && 
 cmake .. && 
 cmake --build . -j$HOST_NCORES && 
 make .
+fi
